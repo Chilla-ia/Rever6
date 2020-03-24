f8df1dced4dc4d40389ab31948a70ea3ec19ec20
Manon Touchard <manon.touchard@epsi.fr>
2020-03-24 12:40:15 +0100

    Dessin du tableau

---
+++ Reversi/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Reversi
+{
+    public class Game
+    {
+        public const int Size = 8;
+        public const int Empty = 0;
+        public const int Black = 1;
+        public const int White = -1;
+
+        private int [,] _board = new int[Size, Size];
+
+        public Game()
+        {
+            _board[Size / 2 - 1, Size / 2 - 1] = _board[Size / 2, Size / 2] = Black;
+            _board[Size / 2 - 1, Size / 2] = _board[Size / 2, Size / 2-1] = White;
+        }
+        public int GetTile(int col, int row) => _board[col, row];
+    }
+}
--- ReversiClient/Program.cs
+++ ReversiClient/Program.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using Reversi;
+using System;
 
 namespace ReversiClient
 {
     class Program
     {
+        private static void DrawLine()
+        {
+            Console.Write("   ");
+            for (var i = 0; i < Game.Size; i++)
+            { 
+                Console.Write("+---");
+            }
+            Console.WriteLine("+");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var game = new Game();
+
+            Console.Write("   ");
+            for (int col = 0; col < Game.Size; col++)
+            {
+                Console.Write($"  { (char)('A' + col) } ");
+            }
+            Console.WriteLine("");
+
+            for (int row = 0; row < Game.Size; row++)
+            {
+                DrawLine();
+                Console.Write($" {row + 1} ");
+                for (int col = 0; col < Game.Size; col++)
+                {
+                    var tile = '?';
+                    switch (game.GetTile(col, row))
+                    {
+                        case Game.Empty: tile = ' '; break;
+                        case Game.Black: tile = 'X'; break;
+                        case Game.White: tile = 'O'; break;
+                    }
+                    Console.Write($"| { tile } ");
+                }
+                Console.WriteLine("|");
+            }
+            DrawLine();
         }
     }
 }
